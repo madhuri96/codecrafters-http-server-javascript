b4f3fbd98097fc198811087e0d9e81b2231afc0f
Madhuri <madhuri.bansode@algorisys.com>
2024-05-14 11:39:50 +0530

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -3,12 +3,19 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 // const server = net.createServer((socket) => {
-//   socket.write("HTTP/1.1 200 OK\r\n\r\n");
-//   socket.on("close", () => {
+//   socket.once("data", (data) => {
+//     const request = data.toString();
+//     const lines = request.split("\r\n");
+//     const [method, path] = lines[0].split(" ");
+
+//     if (path === "/") {
+//       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+//     } else {
+//       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+//     }
+
 //     socket.end();
-//     server.close();
 //   });
 // });
 
@@ -18,8 +25,20 @@ const server = net.createServer((socket) => {
     const lines = request.split("\r\n");
     const [method, path] = lines[0].split(" ");
 
-    if (path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    if (method === "GET" && path.startsWith("/echo/")) {
+      const str = decodeURIComponent(path.substring(6));
+      const responseBody = str;
+      const contentLength = Buffer.byteLength(responseBody, "utf-8");
+
+      const responseHeaders = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: text/plain",
+        `Content-Length: ${contentLength}`,
+        "",
+        responseBody,
+      ].join("\r\n");
+
+      socket.write(responseHeaders);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
