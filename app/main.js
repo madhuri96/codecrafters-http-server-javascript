313f8091b9d65f06a4b7016ee4f728187ec042ee
Madhuri <madhuri.bansode@algorisys.com>
2024-05-14 11:23:19 +0530

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -4,11 +4,27 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
+// const server = net.createServer((socket) => {
+//   socket.write("HTTP/1.1 200 OK\r\n\r\n");
+//   socket.on("close", () => {
+//     socket.end();
+//     server.close();
+//   });
+// });
+
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("close", () => {
+  socket.once("data", (data) => {
+    const request = data.toString();
+    const lines = request.split("\r\n");
+    const [method, path] = lines[0].split(" ");
+
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+
     socket.end();
-    server.close();
   });
 });
 
