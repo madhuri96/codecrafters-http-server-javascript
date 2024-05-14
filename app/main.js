d1010a599c87c0b0741ee0c98f7fe369b74efc82
Madhuri <madhuri.bansode@algorisys.com>
2024-05-14 13:17:30 +0530

    pass 6th stage

--- app/main.js
+++ app/main.js
@@ -24,6 +24,19 @@ console.log("Logs from your program will appear here!");
 //       ].join("\r\n");
 
 //       socket.write(responseHeaders + "\r\n" + responseBody);
+//     } else if (url === "/user-agent") {
+//       const userAgent = lines.find((line) => line.startsWith("User-Agent: "));
+//       const userAgentValue = userAgent ? userAgent.split(": ")[1] : "Unknown";
+//       const contentLength = Buffer.byteLength(userAgentValue, "utf-8");
+
+//       const responseHeaders = [
+//         "HTTP/1.1 200 OK",
+//         "Content-Type: text/plain",
+//         `Content-Length: ${contentLength}`,
+//         "",
+//       ].join("\r\n");
+
+//       socket.write(responseHeaders + "\r\n" + userAgentValue);
 //     } else {
 //       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 //     }
@@ -31,21 +44,21 @@ console.log("Logs from your program will appear here!");
 //     socket.end();
 //   });
 
-//   // Error Handling
 //   socket.on("error", (e) => {
 //     console.error("ERROR: " + e);
 //     socket.end();
 //   });
 // });
 
-const server = net.createServer((socket) => {
-  socket.once("data", (data) => {
+const server = net.createServer({ keepAlive: true }, (socket) => {
+  socket.on("data", (data) => {
     const request = data.toString();
     const lines = request.split("\r\n");
     const [method, url] = lines[0].split(" ");
 
     if (url === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.end();
     } else if (url.startsWith("/echo/")) {
       const content = url.split("/echo/")[1];
       const responseBody = content;
@@ -59,6 +72,7 @@ const server = net.createServer((socket) => {
       ].join("\r\n");
 
       socket.write(responseHeaders + "\r\n" + responseBody);
+      socket.end();
     } else if (url === "/user-agent") {
       const userAgent = lines.find((line) => line.startsWith("User-Agent: "));
       const userAgentValue = userAgent ? userAgent.split(": ")[1] : "Unknown";
@@ -72,11 +86,11 @@ const server = net.createServer((socket) => {
       ].join("\r\n");
 
       socket.write(responseHeaders + "\r\n" + userAgentValue);
+      socket.end();
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.end();
     }
-
-    socket.end();
   });
 
   socket.on("error", (e) => {
